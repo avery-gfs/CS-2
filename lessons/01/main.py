5789eb6a659e8aba583b68e151a8b00285722d3b
Avery <anortonsmith@germantownfriends.org>
2024-09-26 12:39:57 -0400

    notes

--- lessons/01/main.py
+++ lessons/01/main.py
@@ -1,5 +1,5 @@
 def gcd(a, b):
-  div = max(a, b)
+  div = min(a, b)
 
   while a % div != 0 or b % div != 0
     div -= 1
@@ -8,15 +8,23 @@ def gcd(a, b):
 
 class Fraction:
   def __init__(self, num, denom):
+    # add num and denom as fields
+    pass
 
   def value(self):
+    # calculate num / denom
     return None
 
   def simplify(self):
+    # return a new Fraction with simplified
+    # numerator and denominator
     return None
 
   def multiply(self, n):
+    # return a new Fraction that stores the value
+    # of the old fraction multiplied by n
     return None
 
   def __repr__(self):
+    # update to give a good representation of Fraction
     return f"Fraction"
