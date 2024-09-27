dd50f0e91083b572b028decfd4e8f965a420deb2
Avery <anortonsmith@germantownfriends.org>
2024-09-27 14:51:12 -0400

    add tests to problems

--- lessons/01/main.py
+++ lessons/01/main.py
@@ -16,7 +16,27 @@ class Fraction:
     # of the old fraction multiplied by n
     return None
 
-  def simplifed(self):
+  def simplified(self):
     # return a new Fraction with simplified
     # numerator and denominator
     return None
+
+f = Fraction(4, 10)
+
+print("Test __repr__ method:")
+print(f)
+
+print("-" * 10)
+
+print("Test value method:")
+print(f.value())
+
+print("-" * 10)
+
+print("Test multiply method:")
+print(f.multiply(5))
+
+print("-" * 10)
+
+print("Test simplified method:")
+print(f.simplified())
